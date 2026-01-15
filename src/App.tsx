7351c1d4e6f5eb974d915404a91dd3d0525d8efd
Kim Young Jin <kimyoungjin1001@gmail.com>
2026-01-15 22:18:12 +0800

    Add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import { Library } from './components/Library';
 import { BookReader } from './components/BookReader';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Library />} />
-        <Route path="/read/:bookId" element={<BookReader />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Library />} />
+          <Route path="/read/:bookId" element={<BookReader />} />
+        </Routes>
+      </BrowserRouter>
+      <Analytics />
+    </>
   );
 }
 
